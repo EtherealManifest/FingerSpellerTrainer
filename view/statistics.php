12be454334e5d8a009cb51e547a958e89cd41bc0
EtherealManifest <richmondjacobg@gmail.com>
2024-03-18 22:42:09 -0500

    Changed the output for stats to a table

--- view/statistics.php
+++ view/statistics.php
@@ -1,18 +1,24 @@
 <?php include 'header.php'; ?>
-
+<table>
+    <tr>
+        <th>LETTER</th>
+        <th>ID</th>
+        <th>Times Signed</th>
+        <th>Haste</th>
+        <th>Tarry</th>
+        <th>Average</th>
+    </tr>
 <?php foreach($letter_data as $letter){ ?>
+    <tr>
     <?php foreach($letter as $attribute){ ?>
-        <div id = 'stats'>
-            <p class = 'letter'> <?=$attribute["letter"]?> </p>
-            <p class= 'attribute'>Letter ID: <?=$attribute["letter_id"]?></p>
-            <p class = 'data'>Number of Times of Signed: <?=$attribute["frequency"]?></p>
-            <p class = 'data'>Fastest Time: <?=$attribute["haste"]?></p>
-            <p class = 'data'>Slowest Time: <?=$attribute["tarry"]?></p>
-            <p class = 'data'>Average Time: <?=$attribute["average"]?></p>
-        <br>
-    </div>
+            <td class = 'letter'><?=$attribute["letter"]?> </td>
+            <td class= 'attribute'><?=$attribute["letter_id"]?></td>
+            <td class = 'data'><?=$attribute["frequency"]?></td>
+            <td class = 'data'><?=$attribute["haste"]?></td>
+            <td class = 'data'><?=$attribute["tarry"]?></td>
+            <td class = 'data'><?=$attribute["average"]?></td>
     <?php } ?>
-
+    </tr>
 <?php } ?>
-
+    </table>
 <?php include 'footer.php'; ?>
