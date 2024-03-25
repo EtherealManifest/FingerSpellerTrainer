d717b32ead77591af431b036a8dda1d9b5115d02
EtherealManifest <richmondjacobg@gmail.com>
2024-03-25 00:48:22 -0500

    Added autofocus for new forms

--- view/addString.php
+++ view/addString.php
@@ -3,7 +3,7 @@
 <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
     <input type="hidden" name="action" value="add_new_string">
     <label for="new_String">String to Add: </label>
-    <input type="text" id="new_string" name="new_string" required>
+    <input type="text" id="new_string" name="new_string" required autofocus>
     <button type="submit">Add!</button>
 </form>
 
--- view/addWord.php
+++ view/addWord.php
@@ -3,7 +3,7 @@
 <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
     <input type="hidden" name="action" value="add_word">
     <label for="new_Word">Word to Add: </label>
-    <input type="text" id="new_Word" name="new_Word" required>
+    <input type="text" id="new_Word" name="new_Word" required autofocus>
     <button type="submit">Add!</button>
 </form>
 
